5dc7faa87bd8471a4797f639552df53e4f2dc71e
umithavarehst <umithavare@hstmobil.com.tr>
2023-11-16 10:56:03 +0300

    init

---
+++ VueRedirectProject/Controllers/AnswerModel.cs
@@ -0,0 +1,17 @@
+﻿namespace VueRedirectProject.Controllers
+{
+    public class AnswerModel
+    {
+        public string? orderId { get; set; }
+        public string? paymentId { get; set; }
+        public string? random { get; set; }
+        public string? status { get; set; }
+        public string? amount { get; set; }
+        public string? hash { get; set; }
+        public DateTime? operationDate { get; set; }
+        public string? errorCode { get; set; }
+        public string? errorMessage { get; set; }
+        public string? bankAuthCode { get; set; }
+    }
+
+}
---
+++ VueRedirectProject/Controllers/RedirectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VueRedirectProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RedirectController : ControllerBase
+    {
+
+        [HttpPost("Receive")]
+        public IActionResult Receive([FromForm] AnswerModel answerModel)
+        {
+            var url = "http://testhazirsatis.hstmobil.com.tr:8081/paymentresult";
+            var queryString = $"orderId={answerModel.orderId}&paymentId={answerModel.paymentId}";
+            //answerModel to querystring
+            return Redirect($"{url}?{queryString}");
+        }
+    }   
+}
